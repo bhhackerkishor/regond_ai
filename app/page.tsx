49b23ef8fa95809bb6150e6837455ee746d48c7a
kishore kumar <kishornaveen2193@GMAIL.COM>
2025-03-31 15:47:17 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/home/hero" 
 import Section from "./components/home/section" 
+import CTASection from "@/components/ghibliComponent";
 
 export default function Home() {
   return (
     <>
     
     <Hero/>
+    <CTASection/>
     <Section/>
     
     
